a91aa562d091c5e122a4ce90f0fa83d667b6b1cd
edward <edwardlfish@gmail.com>
2026-05-21 12:27:44 +0300

    Refactor: Integrate dynamic character management in DreamTeam and Hero components, update constants for character details

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,3 +1,5 @@
+import {characters} from "../utils/constants.ts";
+
 interface FriendProps {
     friend: string;
     pos: number;
@@ -14,7 +16,7 @@ const Friend = ({friend, pos}: FriendProps) => {
     }
 
     return (
-        <img className={styles} src={friend} alt="Friend"/>
+        <img className={styles} src={characters[friend].img} alt={characters[friend].name}/>
     )
 }
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -16,58 +16,58 @@ export const characters: Record<string, Hero> = {
     luke: {
         name: "Luke Skywalker",
         img: friend0,
-        url: `${baseUrl+version}/peoples/1`
+        url: `${baseUrl + version}/peoples/1`
     },
-    c3po:{
+    c3po: {
         name: "C-3PO",
         img: friend2,
-        url: `${baseUrl+version}/peoples/2`
+        url: `${baseUrl + version}/peoples/2`
     },
-    r2d2:{
+    r2d2: {
         name: "R2-D2",
         img: friend1,
-        url: `${baseUrl+version}/peoples/3`
+        url: `${baseUrl + version}/peoples/3`
     },
-    leia:{
+    leia: {
         name: "Leia Organa",
         img: friend9,
-        url: `${baseUrl+version}/peoples/5`
+        url: `${baseUrl + version}/peoples/5`
     },
-    obi_wan:{
+    obi_wan: {
         name: "Obi-Wan Kenobi",
         img: friend8,
-        url: `${baseUrl+version}/peoples/10`
+        url: `${baseUrl + version}/peoples/10`
     },
-    chewbacca:{
+    chewbacca: {
         name: "Chewbacca",
         img: friend4,
-        url: `${baseUrl+version}/peoples/13`
+        url: `${baseUrl + version}/peoples/13`
     },
-    han_solo:{
+    han_solo: {
         name: "Han Solo",
         img: friend5,
-        url: `${baseUrl+version}/peoples/14`
+        url: `${baseUrl + version}/peoples/14`
     },
-    yoda:{
+    yoda: {
         name: "Yoda",
         img: friend6,
-        url: `${baseUrl+version}/peoples/0`
+        url: `${baseUrl + version}/peoples/0`
     },
-    ewok:{
+    ewok: {
         name: "Wicket Systri Warrick",
         img: friend3,
-        url: `${baseUrl+version}/peoples/30`
+        url: `${baseUrl + version}/peoples/30`
     },
-    falcon:{
+    falcon: {
         name: "Millennium Falcon",
         img: friend7,
-        url: `${baseUrl+version}/transports/10`
+        url: `${baseUrl + version}/transports/10`
     }
 };
 export const defaultHero = 'luke';
 export const periodMonth = 30 * 24 * 60 * 60 * 1000;
 export const navItems = ['Home', 'About me', 'Star Wars', 'Contact'];
-export const friends = [friend1, friend2, friend3, friend4, friend5, friend6, friend7, friend8, friend9];
+export const friends = Object.keys(characters)
 export const starWarsInfo = `Star Wars is an American media franchise created by George Lucas. The space opera saga began with the original Star Wars film (1977) and quickly became a worldwide pop culture phenomenon. It has expanded into various films and other media, including television series, video games, novels, comic books, theme park attractions, and themed areas, comprising an all-encompassing fictional universe. Star Wars is one of the highest-grossing media franchises of all time.
 
 The original film, later retitled Episode IV: A New Hope, was followed by the sequels Episode V: The Empire Strikes Back (1980) and Episode VI: Return of the Jedi (1983), forming the original Star Wars trilogy. Lucas later returned to the series to write and direct a prequel trilogy, consisting of Episode I: The Phantom Menace (1999), Episode II: Attack of the Clones (2002), and Episode III: Revenge of the Sith (2005). In 2012, Lucas sold his production company to Disney, relinquishing his ownership of the franchise. This led to a sequel trilogy, consisting of Episode VII: The Force Awakens (2015), Episode VIII: The Last Jedi (2017), and Episode IX: The Rise of Skywalker (2019).
